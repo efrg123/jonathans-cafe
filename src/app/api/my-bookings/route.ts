69cd41aff3de3d71712247c4cd6656488153f6cc
Round People Studio <fahd.faiyaz@gmail.com>
2025-08-10 20:07:36 +0500

    chore: build user auth

--- src/app/api/my-bookings/route.ts
+++ src/app/api/my-bookings/route.ts
@@ -1,6 +1,6 @@
 ﻿// src/app/api/my-bookings/route.ts
 import { NextResponse } from 'next/server';
-import { createServerClient } from '@supabase/ssr';
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 import { prisma } from '@/lib/prisma';
 
